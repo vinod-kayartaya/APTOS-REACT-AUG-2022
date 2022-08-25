ad60d6b690f4eda3e06b3374cd08f618fcd0645e
Vinod KK <vinod@vinod.co>
2022-08-25 16:36:19 +0530

    🇮🇳

--- react-router-redux-saga-customers-app/src/components/CustomerDetails.tsx
+++ react-router-redux-saga-customers-app/src/components/CustomerDetails.tsx
@@ -5,7 +5,7 @@ import { StoreType } from '../datatypes/storeTypes';
 import { fetchOneCustomer } from '../redux/customerActions';
 
 const CustomerDetails = () => {
-    const { customer } = useSelector(
+    const { customer, loading } = useSelector(
         (store: StoreType) => store.customerReducer
     );
     const params: any = useParams();
@@ -18,39 +18,45 @@ const CustomerDetails = () => {
 
     return (
         <>
-            <h3>
-                <img
-                    src={customer.avatar}
-                    alt={customer.firstname}
-                    className='img-thumnail'
-                />
-                {customer.gender === 'Male' ? 'Mr.' : 'Ms.'}{' '}
-                {customer.firstname} {customer.lastname}
-            </h3>
-            <table className='table' style={{ width: '600px' }}>
-                <tbody>
-                    <tr>
-                        <td>Firstname</td>
-                        <td>{customer.firstname}</td>
-                    </tr>
-                    <tr>
-                        <td>Lastname</td>
-                        <td>{customer.lastname}</td>
-                    </tr>
-                    <tr>
-                        <td>Email address</td>
-                        <td>{customer.email}</td>
-                    </tr>
-                    <tr>
-                        <td>City</td>
-                        <td>{customer.city}</td>
-                    </tr>
-                    <tr>
-                        <td>Gender</td>
-                        <td>{customer.gender}</td>
-                    </tr>
-                </tbody>
-            </table>
+            {loading ? (
+                'Loading... pls wait!'
+            ) : (
+                <div>
+                    <h3>
+                        <img
+                            src={customer.avatar}
+                            alt={customer.firstname}
+                            className='img-thumnail'
+                        />
+                        {customer.gender === 'Male' ? 'Mr.' : 'Ms.'}{' '}
+                        {customer.firstname} {customer.lastname}
+                    </h3>
+                    <table className='table' style={{ width: '600px' }}>
+                        <tbody>
+                            <tr>
+                                <td>Firstname</td>
+                                <td>{customer.firstname}</td>
+                            </tr>
+                            <tr>
+                                <td>Lastname</td>
+                                <td>{customer.lastname}</td>
+                            </tr>
+                            <tr>
+                                <td>Email address</td>
+                                <td>{customer.email}</td>
+                            </tr>
+                            <tr>
+                                <td>City</td>
+                                <td>{customer.city}</td>
+                            </tr>
+                            <tr>
+                                <td>Gender</td>
+                                <td>{customer.gender}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            )}
         </>
     );
 };
